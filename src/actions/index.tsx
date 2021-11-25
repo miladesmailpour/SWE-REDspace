e12f5092785e11c3b852a43750b2c8d7d36cd4c9
milad <milad.esmailpour@gmail.com>
2021-11-24 23:59:32 -0500

    handeling data pagination, coming from SWAPI

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,55 +1,68 @@
 import axios from "axios";
 import * as url from "../urlPath";
 
-// export const getPeople = async (
-//   dispatch: any,
-//   page: number = 1
-// ): Promise<[] | undefined> => {
-//   const query = `${url.peopleUrl}/?page=${page}`;
-//   const response = await axios.get(query);
-//   const data = response.data;
-
-//   if (data[0].next > page) {
-//     return data.concat(await getPeople(page + 1));
-//   } else {
-//     return async (dispatch:any) => {
-//       dispatch({
-//       type: "GET_PEOPLE",
-//       payload: response.data.results,
-//     });
-//     }
-//   }
-// };
-
 export const getPeople = () => {
   return async (dispatch: any, getstate: any) => {
-    const response = await axios.get(url.peopleUrl);
+    const allPoeple = async (page: number = 1): Promise<[]> => {
+      const query = `${url.peopleUrl}/?page=${page}&format=json`;
+      const res = await axios.get(query);
+      const data = res.data;
+
+      if (data.next !== null) {
+        return data.results.concat(await allPoeple(page + 1));
+      } else {
+        return data.results;
+      }
+    };
+    const response = await allPoeple();
 
     dispatch({
       type: "GET_PEOPLE",
-      payload: response.data.results,
+      payload: response,
     });
   };
 };
 
 export const getPlanets = () => {
   return async (dispatch: any, getstate: any) => {
-    const response = await axios.get(url.planetsUrl);
+    const allPlanets = async (page: number = 1): Promise<[]> => {
+      const query = `${url.planetsUrl}/?page=${page}&format=json`;
+      const res = await axios.get(query);
+      const data = res.data;
+
+      if (data.next !== null) {
+        return data.results.concat(await allPlanets(page + 1));
+      } else {
+        return data.results;
+      }
+    };
+    const response = await allPlanets();
 
     dispatch({
       type: "GET_PLANETS",
-      payload: response.data.results,
+      payload: response,
     });
   };
 };
 
 export const getMovies = () => {
   return async (dispatch: any, getstate: any) => {
-    const response = await axios.get(url.moviesUrl);
+    const allMovies = async (page: number = 1): Promise<[]> => {
+      const query = `${url.moviesUrl}/?page=${page}&format=json`;
+      const res = await axios.get(query);
+      const data = res.data;
+
+      if (data.next !== null) {
+        return data.results.concat(await allMovies(page + 1));
+      } else {
+        return data.results;
+      }
+    };
+    const response = await allMovies();
 
     dispatch({
       type: "GET_MOVIES",
-      payload: response.data.results,
+      payload: response,
     });
   };
 };
