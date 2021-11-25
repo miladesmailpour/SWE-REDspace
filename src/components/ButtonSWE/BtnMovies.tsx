353c83f1617ce38ec5d4260c9dead380cea67716
milad <milad.esmailpour@gmail.com>
2021-11-25 00:48:40 -0500

    preloading happends in the first time home call

--- src/components/ButtonSWE/BtnMovies.tsx
+++ src/components/ButtonSWE/BtnMovies.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { getMovies } from "../../actions";
 import BtnHOC from "../hocs/BtnHOC";
-import { Button, StyleSheet, View } from "react-native";
 
 const BtnMovies = (navigation: any) => {
-  const dispatch = useDispatch();
   const handlerClick = () => {
-    dispatch(getMovies());
     navigation.navigate("Movies");
   };
   return <BtnHOC title="Movies" onClick={handlerClick} />;
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ViewHOC from "../hocs/ViewHOC";
 import BtnMovies from "../ButtonSWE/BtnMovies";
 import BtnPeople from "../ButtonSWE/BtnPeople";
 import BtnPlanets from "../ButtonSWE/BtnPlanets";
 import { View, Button, StyleSheet } from "react-native";
+import { useDispatch } from "react-redux";
+import { getMovies, getPeople, getPlanets } from "../../actions";
 
 export type HomeProps = {
   navigation: any;
 };
 const Home = ({ navigation }: HomeProps) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMovies());
+    dispatch(getPeople());
+    dispatch(getPlanets());
+  }, []);
+
   const combiner = () => {
     return [
       BtnPeople(navigation),
